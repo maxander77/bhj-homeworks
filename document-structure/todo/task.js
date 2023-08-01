821c8de6530685713c7570948f7d5d60784dd842
Maxander <maxander77@gmail.com>
2023-08-01 19:23:10 +0200

    first commit todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById('tasks__add');
+const inputField = document.getElementById('task__input');
+const taskList = document.getElementById('tasks__list');
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const taskText = inputField.value;
+  const newTask = document.createElement('div');
+  newTask.classList.add('task');
+
+  const taskTitle = document.createElement('div');
+  taskTitle.classList.add('task__title');
+  taskTitle.textContent = taskText;
+
+  const taskRemove = document.createElement('a');
+  taskRemove.classList.add('task__remove');
+  taskRemove.innerHTML = '&times;';
+
+  taskRemove.addEventListener('click', () => {
+    newTask.remove();
+  });
+
+  newTask.appendChild(taskTitle);
+  newTask.appendChild(taskRemove);
+
+  taskList.appendChild(newTask);
+
+  inputField.value = '';
+ 
+
+})
