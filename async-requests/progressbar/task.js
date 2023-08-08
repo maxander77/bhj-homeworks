ca46aebaf2bcc0830ba848d722bc0b312e8b5ece
Maxander <maxander77@gmail.com>
2023-08-08 16:28:24 +0200

    First commit progressbar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  const progressBar = document.getElementById('progress');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.upload.addEventListener('progress', (event) => {
+      if (event.lengthComputable) {
+        const progress = (event.loaded / event.total) * 100;
+        progressBar.value = progress;
+      }
+    });
+
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === xhr.DONE) {
+        if (xhr.status === 200) {
+          alert('Success!');
+        } else {
+          alert('Error upload');
+        }
+      }
+    });
+
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.send(formData);
+  });
+});
